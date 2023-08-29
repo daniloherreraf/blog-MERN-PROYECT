12f60ecc8566378668bc8bbeb364a3346d658383
daniloherreraf <herreraf.danilo@gmail.com>
2023-08-29 11:03:58 -0500

    create model Post

--- server/index.js
+++ server/index.js
@@ -1,11 +1,6 @@
-import express from 'express';
-import postsRoutes from './routes/posts.routes.js';
+import app from './app.js';
 import { connectDB } from './db.js';
 
-const app = express();
 connectDB();
-
-app.use(postsRoutes);
-
 app.listen(3000);
 console.log('Server in running port', 3000);
