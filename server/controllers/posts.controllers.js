8624ecc73f1d529f9ff8c78ba5d18400245788a9
daniloherreraf <herreraf.danilo@gmail.com>
2023-08-30 15:55:24 -0500

    error handling

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,35 +1,53 @@
 import Post from '../models/Post.js';
 
 export const getPosts = async (req, res) => {
-  const posts = await Post.find();
-  res.send(posts);
+  try {
+    const posts = await Post.find();
+    res.send(posts);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const createPost = async (req, res) => {
-  const { tittle, description } = req.body;
-  const newPost = new Post({ tittle, description });
-  await newPost.save();
-  return res.json(newPost);
+  try {
+    const { tittle, description } = req.body;
+    const newPost = new Post({ tittle, description });
+    await newPost.save();
+    return res.json(newPost);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const updatePost = async (req, res) => {
-  const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  if (!post) return res.sendStatus(404);
-  return res.json(post);
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!post) return res.sendStatus(404);
+    return res.json(post);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const deletePost = async (req, res) => {
-  const postRemoved = await Post.findByIdAndDelete(req.params.id);
-
-  if (!postRemoved) return res.sendStatus(404);
-
-  return res.sendStatus(204);
+  try {
+    const postRemoved = await Post.findByIdAndDelete(req.params.id);
+    if (!postRemoved) return res.sendStatus(404);
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
-export const getByIdPost = async(req, res) => {
-  const post = await Post.findById(req.params.id)
-  if (!post) return res.sendStatus(404);
-  return res.json(post)
-}
+export const getByIdPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.sendStatus(404);
+    return res.json(post);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
