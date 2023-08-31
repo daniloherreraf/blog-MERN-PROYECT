08221b737f09730c7d237eb16e7f36356dbd6ea4
daniloherreraf <herreraf.danilo@gmail.com>
2023-08-31 18:25:46 -0500

    create folder client install dependencies

--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ import app from './app.js';
 import { connectDB } from './db.js';
 
 connectDB();
-app.listen(3000);
-console.log('Server in running port', 3000);
+app.listen(5000);
+console.log('Server in running port', 5000);
