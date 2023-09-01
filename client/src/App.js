3c95f588df3935f93a9354b9d3774e753c46471b
daniloherreraf <herreraf.danilo@gmail.com>
2023-09-01 13:53:46 -0500

    create pages config tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Homepage from './pages/Homepage';
+import PostForm from './pages/PostForm';
+import NotFoundPage from './pages/NotFoundPage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-cyan-700 min-h-screen flex items-center">
+      <div className="px-10 container m-auto">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/new" element={<PostForm />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
